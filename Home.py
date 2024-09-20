63dcae9bde7674f3f90a05cf7bfc76d7fb4b84e4
Samyak <samyak15.sj@gmail.com>
2024-09-20 21:36:45 +0530

    Initial commit with LFS tracking

---
+++ Home.py
@@ -0,0 +1,11 @@
+import sklearn
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
---
+++ latlong_scraper.py
@@ -0,0 +1,42 @@
+import warnings
+from urllib3.exceptions import NotOpenSSLWarning
+
+warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Base URL for Google Search
+BASE_URL = "https://www.google.com/search?q="
+
+# Headers to simulate a real browser visit
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
+# Function to scrape latitude and longitude
+def get_coordinates(sector):
+    search_term = f"sector {sector} gurgaon longitude & latitude"
+    response = requests.get(BASE_URL + search_term, headers=HEADERS)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        coordinates_div = soup.find("div", class_="Z0LcW t2b5Cf")
+        if coordinates_div:
+            return coordinates_div.text
+    return None
+
+# Create a list to hold data
+data = []
+
+# Iterate over sectors and fetch coordinates
+for sector in range(1, 116):
+    coordinates = get_coordinates(sector)
+    data.append({"Sector": f"Sector {sector}", "Coordinates": coordinates})
+
+# Convert list to DataFrame
+df = pd.DataFrame(data)
+
+# Save DataFrame
+df.to_csv("gurgaon_sectors_coordinates.csv", index=False)
